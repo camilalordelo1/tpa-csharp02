52fbc3f8c976c28706719dc7088ef036983cb80e
Camila Lordelo <64118360+camilalordelo1@users.noreply.github.com>
2022-01-03 13:14:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variavel do tipo real
+            double X;
+            double Y;
+            double XY;
+            // entrada de dados
+            Console.WriteLine("Digite N1: "); //escreval
+            X = double.Parse(Console.ReadLine()); //leia
+            Console.WriteLine("Digite N2: "); //escreval
+            Y = double.Parse(Console.ReadLine());
+            XY = X + Y;
+
+            //saida de dados
+            Console.WriteLine("Soma: " + XY);
+        }
+    }
+}
